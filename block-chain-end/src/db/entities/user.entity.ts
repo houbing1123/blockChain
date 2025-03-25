2bb1a306f90ee2937d91bec08f95b53517e28016
houbing1123 <1604063822@qq.com>
2025-03-25 22:02:59 +0800

    登录模块测试，页面开发

--- block-chain-end/src/db/entities/user.entity.ts
+++ block-chain-end/src/db/entities/user.entity.ts
@@ -1,8 +1,8 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, Column,ObjectIdColumn } from 'typeorm';
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn()
+  @ObjectIdColumn()
   id: number;
 
   @Column({ unique: true })
--- block-chain-front/src/pages/register/Register.tsx
+++ block-chain-front/src/pages/register/Register.tsx
@@ -1,24 +1,51 @@
 import React, { ReactElement } from 'react';
+import { useNavigate } from "react-router-dom";
 import {Input,Button} from "antd"
 
 const Register:React.FC = ():ReactElement => {
+    const [username, setUsername] = React.useState<string>('');
+    const [password, setPassword] = React.useState<string>('');
+    const navigate = useNavigate();
     const register = ():void =>{
         console.log('register')
-        fetch('http://localhost:3000/api/register')
+        fetch('http://localhost:5170/auth/register',{
+            method: 'POST',
+            headers: {  'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        }).then(res => res.json())
+        .then(res => {
+            if(res.code === 200){
+                console.log('注册成功')
+            }else{
+                console.log('注册失败')
+            }
+        }).catch(err => {
+            console.log(err)    
+        })
+    }
+    const toLogin = ():void =>{
+        navigate('/login')
     }
     return (
         <div>
             <h1>Register</h1>
             <div>
-                <Input placeholder='请输入账户'></Input>
+                <Input value={username} onChange={(e)=>setUsername(e.target.value)} placeholder='请输入账户'></Input>
             </div>
             <br />
             <div>
-                <Input placeholder='请输入密码' type='password'></Input>
+                <Input value={password} onChange={(e)=>setPassword(e.target.value)} placeholder='请输入密码' type='password'></Input>
             </div>
             <br />
-            <div>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'space-between'
+            }}>
                 <Button type='primary' onClick={register}>注册</Button>
+                <Button type='primary' onClick={toLogin}>登录</Button>
             </div>
             
         </div>
